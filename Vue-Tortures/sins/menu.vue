54e5096a6cc76b36bc9c8173d49d6da4bbfbe79e
марина марина <tima.d.sor@yandex.ru>
2024-11-03 19:42:46 +0300

    aaab

--- Vue-Tortures/sins/menu.vue
+++ Vue-Tortures/sins/menu.vue
@@ -15,5 +15,14 @@ const hellotion = ref('hellotion')
 .hellotion {
     text-align: center;
     background-color: rgb(18, 18, 18);
+    padding: 2%;
+}
+
+h1 {
+    margin: 0;
+}
+body {
+    font-family: "Comic Sans MS";
+    margin: 0;
 }
 </style>
