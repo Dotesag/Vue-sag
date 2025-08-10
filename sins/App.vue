e1668e308395861ef15faeb9b5e104a1eea25b1c
марина марина <tima.d.sor@yandex.ru>
2025-08-10 17:31:03 +0300

    revolution

---
+++ sins/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref} from 'vue'
+import menu_ from './menu.vue';
+
+</script>
+
+<template>
+  <menu_ />  
+</template>
+
+<style>
+body{
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+  background-color: rgb(14, 14, 14);
+  color: aliceblue ;
+  letter-spacing: 1px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+
+import { createApp } from 'vue'
+import App from '../sins/App.vue'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_FIREBASE_API,
+    authDomain: "vue-torture.firebaseapp.com",
+    projectId: "vue-torture",
+    storageBucket: "vue-torture.firebasestorage.app",
+    messagingSenderId: "804292613626",
+    appId: "1:804292613626:web:0f17e701bb7c40769b8043",
+    measurementId: "G-YY0FKTQKLH"
+  };
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+createApp(App).mount('#app')
